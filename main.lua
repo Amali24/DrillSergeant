0042522e9d8c0148e57e95804ee90241883387cb
Amali24 <apt41790@gmail.com>
2015-10-27 17:42:32 -0400

    Updated with Slag's Edits

--- main.lua
+++ main.lua
@@ -1,15 +1,48 @@
-drillSergeantAddOn = { };
+drillSergeantAddOn = {
+	inRaid=false,
+	roleLines={
+		["damage"]={
+			"It's impressive how bad your DPS is with that gear.",
+		},
+		["healing"]={
+			"You call that healing?",
+			"If you're not careful, both tanks might outheal you there."
+		},
+		["tanking"]={
+			"Do you think it'd be possible to take more unnecessary damage?",
+			"Maybe try popping a cooldown?",
+		},
+	}
+};
+
 local dsa = drillSergeantAddOn;
-dsa.inRaid = IsInRaid(); -- Checks if in raid group
-if (dsa.inRaid == true)then
-	
+
+-- Checks if in raid group
+dsa.inRaid = IsInRaid();
+
+if (dsa.inRaid == true) then
+	-- grab the number of members in the raid
 	dsa.raidSize = GetNumGroupMembers();
-	
-	SendChatMessage("There are " .. dsa.raidSize .. " people in your raid." , "PARTY")
-	dsa name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(1)
-	SendChatMessage("and the first member is a " .. dsa.class .. " named " .. dsa.name .. "." , "PARTY")
+	-- select a random player
+	local index = math.random(dsa.raidSize)
+	-- get information about that player
+	-- role appears to only be MAINTANK or TANK...
+	local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(index)
 
+	-- using player name, we'll want to target the player
+	-- get that player's specialization, and grab the global ID for the spec
+	-- use GetSpecializationRoleByID(int) to figure out if it's tank/dps/hps
+	NotifyInspect(name)
+	local playerSpec = GetInspectSpecialization()
+	local playerSpecRole = GetSpecializationRoleByID(playerSpec)
+
+	SendChatMessage(playerSpecRole, "RAID")
+
+	-- grab a phrase
+	local j = table.getn(dsa["roleLines"][playerSpecRole])
+	local phrase = dsa["roleLines"][playerSpecRole][math.random(0, j-1)]
+		
+	-- SendChatMessage("Hey there, " .. name .. "! " .. phrase, "RAID")
 else
-	message("No");
-
+	message("Not in a raid. Sorry there, chief.");
 end
